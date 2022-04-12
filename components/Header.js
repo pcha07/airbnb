01a42562fe584dd945458d3c5995009d50f4348c
Prabh Chahal <chahalprabh10@gmail.com>
2022-04-12 15:52:44 -0700

    Search functionality

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,56 @@
 import Image from 'next/image'
-import { SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon, UsersIcon } from '@heroicons/react/solid'
+import {
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UsersIcon,
+} from '@heroicons/react/solid'
+import { useState } from 'react'
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css'
+import { DateRangePicker } from 'react-date-range'
+import { useRouter } from 'next/router'
 
-function Header() {
+function Header({ placeholder }) {
+  const [searchInput, setSearchInput] = useState('')
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
+  const [numOfGuests, setNumOfGuests] = useState(1)
+
+  const router = useRouter();
+
+  const selectionRange = {
+    startDate,
+    endDate,
+    key: 'selection',
+  }
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate)
+    setEndDate(ranges.selection.endDate)
+  }
+
+  const resetInput = () => {
+    setSearchInput('')
+  }
+
+  const search = () => {
+    router.push({
+      pathname:'/search',
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        numOfGuests,
+      }
+    })
+  }
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white p-5 shadow-md md:px-10">
       {/* Left */}
-      <div className="relative my-auto flex h-10 cursor-pointer items-center">
+      <div onClick={() => router.push('/')} className="relative my-auto flex h-10 cursor-pointer items-center">
         <Image
           src="https://links.papareact.com/qd3"
           layout="fill"
@@ -17,22 +62,53 @@ function Header() {
       {/* Middle - Search */}
       <div className="flex items-center rounded-full py-2 md:border-2 md:shadow-sm">
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           type="text"
-          placeholder="Start your search"
+          placeholder={placeholder || "Start your search"}
           className="flex-grow bg-transparent pl-5 text-sm text-gray-600 placeholder-gray-400 outline-none"
         />
         <SearchIcon className=" hidden h-8 cursor-pointer rounded-full bg-red-400 p-2 text-white md:mx-2 md:inline-flex" />
       </div>
 
       {/* Right */}
-      <div className='flex items-center space-x-4 justify-end text-gray-500'>
-          <p className='hidden md:inline cursor-pointer'>Become a host</p>
-          <GlobeAltIcon className='h-6 cursor-pointer'/>
-          <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
-              <MenuIcon className='h-6'/>
-              <UserCircleIcon className='h-6'/>
-          </div>
+      <div className="flex items-center justify-end space-x-4 text-gray-500">
+        <p className="hidden cursor-pointer md:inline">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <div className="flex items-center space-x-2 rounded-full border-2 p-2">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
       </div>
+      {searchInput && (
+        <div className="col-span-3 mx-auto flex flex-col">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={['#FD5861']}
+            onChange={handleSelect}
+          />
+          <div className="mb-4 flex items-center border-b">
+            <h2 className="flex-grow text-2xl font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              type="number"
+              className="w-12 pl-2 text-lg text-red-400 outline-none"
+              value={numOfGuests}
+              onChange={(e) => setNumOfGuests(e.target.value)}
+              min={1}
+            />
+          </div>
+          <div className="flex">
+            <button onClick={resetInput} className="flex-grow text-gray-500">
+              Cancel
+            </button>
+            <button onClick={search} className="flex-grow text-red-400">Search</button>
+          </div>
+        </div>
+      )}
     </header>
   )
 }
---
+++ pages/search.js
@@ -0,0 +1,69 @@
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import { useRouter } from 'next/router'
+import { format } from 'date-fns'
+import InfoCard from '../components/InfoCard'
+
+function Search({ searchResults }) {
+  const router = useRouter()
+
+  const { location, startDate, endDate, numOfGuests } = router.query
+
+  const formattedStartDate = format(new Date(startDate), 'dd MMMM yy')
+  const formattedEndDate = format(new Date(endDate), 'dd MMMM yy')
+
+  const range = `${formattedStartDate} - ${formattedEndDate}`
+  return (
+    <div className="h-screen">
+      <Header placeholder={`${location} | ${range} | ${numOfGuests} guests`} />
+
+      <main className="flex">
+        <section className="flex-grow px-6 pt-14">
+          <p className="text-xs">
+            300+ stays - {range} - for {numOfGuests} guests
+          </p>
+          <h1 className="mt-2 mb-6 text-3xl font-semibold">
+            Stays in {location}
+          </h1>
+          <div className="text-gray-8-- hidden space-x-3 whitespace-nowrap lg:inline-flex">
+            <p className="button">Cancellation Flexibilaty</p>
+            <p className="button">Type of Place</p>
+            <p className="button">Price</p>
+            <p className="button">Rooms and Beds</p>
+            <p className="button">More Filters</p>
+          </div>
+          <div className="flex flex-col">
+            {searchResults.map(
+              ({ img, location, title, description, star, price, total }) => (
+                <InfoCard
+                  key={img}
+                  img={img}
+                  location={location}
+                  description={description}
+                  title={title}
+                  star={star}
+                  price={price}
+                />
+              )
+            )}
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Search
+
+export async function getServerSideProps() {
+  const searchResults = await fetch('https://links.papareact.com/isz').then(
+    (res) => res.json()
+  )
+
+  return {
+    props: {
+      searchResults,
+    },
+  }
+}
